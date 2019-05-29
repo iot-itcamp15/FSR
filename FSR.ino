9f98889d5417175ef1d1a88853960268dfcb2ea5
Kritsana Sakulpaisanvaree <spiddy_xx@hotmail.com>
2019-05-29 16:44:44 +0700

    updates code

--- FSR.ino
+++ FSR.ino
@@ -1,19 +1,16 @@
 void setup() {
-  pinMode(A0,OUTPUT); //pinที่รับต้องเป็นpinที่สามารถรับค่าAnalogได้เท่านั้น
-  Serial.begin(115200); //set serialว่าจะให้มันแสดงค่าในช่วงไหน
+  pinMode(A0,INPUT); //pinที่รับต้องเป็นpinที่สามารถรับค่าAnalogได้เท่านั้น
+  Serial.begin(9600); //set serialว่าจะให้มันแสดงค่าในช่วงไหน
 }
 
 void loop() {
-  int val = analogRead(A0);//อ่านค่าที่pin A0ที่ต่ออยู่กับตัวFSR แล้วนำไปเก็บที่ตัวแปล val
-  
-  //แสดงค่าแรงกดใน Serial monitor
-  //ตั้งค่าดีเลย์กันคอมค้าง
-  if(val>500){
+  int val = analogRead(A0);//อ่านค่าที่pin A0ที่ต่ออยู่กับตัว FSR แล้วนำไปเก็บที่ตัวแปล val
+  if(val>200){ //เมื่อมีแรงกดเกิน 500 จะแสดงค่าแรงกดขึ้นมา ในmonitor
     Serial.print("pressure = ");
     Serial.println(val);
-    delay(300);
-  }else{
+    delay(500);
+  }else{ //ถ้าแรงดันน้อยกว่า500 จะแสดงคำว่า No pressure ในmonitor
     Serial.println("No pressure");
-    delay(300);
+    delay(500);
   }
 }
